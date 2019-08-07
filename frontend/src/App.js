ec98271d11867fca590e40fd44d595bd10388af8
daviguelfi <daviguelfi@gmail.com>
2019-08-07 16:58:51 -0300

    Back to login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import './App.css'
+
+import Routes from './routes'
 
 
 function App() {
   return (
-    <div className="">
-      <h1>Hello</h1>
-    </div>
+    <Routes/>
   );
 }
 
